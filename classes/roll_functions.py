097e47c6bd760bbe61277c84920b173a5761107c
ephreal <tjannx@hotmail.com>
2019-07-02 12:26:17 -0700

    Removed commented out single roll

--- classes/roll_functions.py
+++ classes/roll_functions.py
@@ -6,8 +6,5 @@ class roller():
     def __init__(self):
         pass
 
-    # async def single_roll(self, sides):
-    #     return random.randint(1, sides)
-
     async def roll(self, dice_pool=1, sides=6):
         return [random.randint(1, sides) for _ in range(0, dice_pool)]
