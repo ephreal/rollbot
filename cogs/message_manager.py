56526888a409ac7c6ef29e44edb0fca3251424c0
ephreal <tjannx@hotmail.com>
2020-02-24 16:27:39 -0700

    Updated documentation

--- cogs/message_manager.py
+++ cogs/message_manager.py
@@ -1,29 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """
-Copyright 2018-2019 Ephreal
+This software is licensed under the License (MIT) located at
+https://github.com/ephreal/rollbot/Licence
 
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
-
-Commands provided by this cog
-    purge : remove messages from a channel.
+Please see the license for any restrictions or rights granted to you by the
+License.
 """
 
 from discord import Embed, TextChannel
@@ -31,6 +12,13 @@ from discord.ext import commands
 
 
 class MessageManager(commands.Cog):
+    """
+    commands:
+        move(limit, text_channel):
+            moves limit amount of messages from one text channel to another
+        purge(limit):
+            purges limit amount of messages from a channel
+    """
     def __init__(self, bot):
         self.bot = bot
         self.prefix = self.bot.command_prefix
